485414e515542bab21ffba088cbcd4387736baab
Ben Morris <benjaminmjmorris@gmail.com>
2016-11-18 15:17:09 -0500

    removed actionbar, saving pictures, getting location

--- app/src/main/java/com/example/benja/geopix/MainCamera.java
+++ app/src/main/java/com/example/benja/geopix/MainCamera.java
@@ -1,8 +1,6 @@
 package com.example.benja.geopix;
 
 import android.Manifest;
-import android.app.Activity;
-import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -10,16 +8,17 @@ import android.hardware.Camera;
 import android.location.Location;
 import android.location.LocationManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
-import android.support.v4.app.Fragment;
 
 import java.io.DataOutputStream;
 import java.io.File;
@@ -27,7 +26,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -40,9 +38,12 @@ public class MainCamera extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+        //Remove title bar and initialize view
         setContentView(R.layout.activity_camera);
+
         Context context = getApplicationContext();
-        if (!checkCameraHardware(context)){
+        if (!checkCameraHardware(context)) {
             System.exit(0);
         }
         File file = new File(context.getFilesDir(), "testfile");
@@ -60,13 +61,17 @@ public class MainCamera extends AppCompatActivity {
         int permissionCheckWrite = ContextCompat.checkSelfPermission(context, Manifest.permission.WRITE_EXTERNAL_STORAGE);
 
         if (permissionCheckWrite != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE} ,2);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 2);
         }
+
+        if (ContextCompat.checkSelfPermission(context, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+
 
         if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA} ,1);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, 1);
         }
-
 
         Camera mCamera = getCameraInstance();
 
@@ -81,42 +86,41 @@ public class MainCamera extends AppCompatActivity {
 
     }
 
-    private void setupCaptureButton(final Camera mCamera){
+    private void setupCaptureButton(final Camera mCamera) {
         Button captureButton = (Button) findViewById(R.id.button_capture);
         captureButton.setOnClickListener(
-            new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // get an image from the camera
-                    try {
-                        mCamera.takePicture(null, null, mPicture);
-                        mCamera.stopPreview();
-                        mCamera.startPreview();
-                    }catch(Exception e){
-                        Log.d("something","else");
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        try {
+                            Log.d("sertupCaptureButton", "Taking Photo");
+                            mCamera.takePicture(null, null, mPicture);
+                        } catch (Exception e) {
+                            Log.d("something", "else");
+                        }
                     }
-                }
-            });
+                });
     }
 
-    private void setupMapButton(final Context mContext){
+    private void setupMapButton(final Context mContext) {
         Button mapButton = (Button) findViewById(R.id.button_map);
         mapButton.setOnClickListener(
                 new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // get an image from the camera
-                try {
-                    Intent startIntent = new Intent(mContext, MapsActivity.class);
-                    mContext.startActivity(startIntent);
-                }catch(Exception e){
-                    Log.d("something","else");
-                }
-            }
-        });
+                    @Override
+                    public void onClick(View v) {
+                        // get an image from the camera
+                        try {
+                            Intent startIntent = new Intent(mContext, MapsActivity.class);
+                            mContext.startActivity(startIntent);
+                        } catch (Exception e) {
+                            Log.d("something", "else");
+                        }
+                    }
+                });
     }
 
-    private void postPic(){
+    private void postPic() {
 
     }
 
@@ -124,33 +128,41 @@ public class MainCamera extends AppCompatActivity {
 
         @Override
         public void onPictureTaken(byte[] data, Camera camera) {
+            Log.d("cam", "OnPictureTaken has been called ");
 
             File pictureFile = getOutputMediaFile(MEDIA_TYPE_IMAGE);
-            if (pictureFile == null){
+            if (pictureFile == null) {
                 Log.d("cam", "Error creating media file, check storage permissions: ");
                 return;
             }
 
             try {
                 //TODO make this async
+                double lat = getLocation().getLatitude();
+                double lon = getLocation().getLongitude();
 //                uploadPicture(data);
+                Log.d("LocationManager", "lat: " + lat + ", lon: " + lon);
                 FileOutputStream fos = new FileOutputStream(pictureFile);
                 fos.write(data);
+                fos.flush();
                 fos.close();
             } catch (FileNotFoundException e) {
                 Log.d("cam", "File not found: " + e.getMessage());
             } catch (IOException e) {
                 Log.d("cam", "Error accessing file: " + e.getMessage());
             }
+            camera.stopPreview();
+            camera.startPreview();
         }
     };
 
     private void uploadPicture(byte[] pixels) throws IOException {
+        Location currentLocation = getLocation();
         String attachmentName = "jpeg";
         String attachmentFileName = "jpeg.jpeg";
         String crlf = "\r\n";
         String twoHyphens = "--";
-        String boundary =  "*****";
+        String boundary = "*****";
 
         HttpURLConnection httpUrlConnection = null;
         URL url = new URL("http://example.com/server.cgi");
@@ -183,6 +195,13 @@ public class MainCamera extends AppCompatActivity {
         request.close();
     }
 
+    private Location getLocation() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return null;
+        }
+        return locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+    }
+
     private boolean checkCameraHardware(Context context) {
         if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
             // this device has a camera
@@ -197,7 +216,6 @@ public class MainCamera extends AppCompatActivity {
     public static Camera getCameraInstance(){
         Camera c = null;
 
-
         try {
             c = Camera.open(); // attempt to get a Camera instance
         }
@@ -207,8 +225,6 @@ public class MainCamera extends AppCompatActivity {
         }
         return c; // returns null if camera is unavailable
     }
-
-
 
     public static final int MEDIA_TYPE_IMAGE = 1;
     public static final int MEDIA_TYPE_VIDEO = 2;
@@ -223,8 +239,10 @@ public class MainCamera extends AppCompatActivity {
         // To be safe, you should check that the SDCard is mounted
         // using Environment.getExternalStorageState() before doing this.
 
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+//        File mediaStorageDir = new File(Environment.DIRECTORY_PICTURES, "MyCameraApp");
+
+        final File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES)
+                .getAbsolutePath(), "/MyCameraApp");
 
         // This location works best if you want the created images to be shared
         // between applications and persist after your app has been uninstalled.
@@ -249,7 +267,6 @@ public class MainCamera extends AppCompatActivity {
         } else {
             return null;
         }
-
         return mediaFile;
     }
 }
