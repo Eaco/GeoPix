2ebcf28cf7555e0720b87889c8160c1070d6b2ec
Ben Morris <benjaminmjmorris@gmail.com>
2016-11-21 23:45:54 -0500

    moving photogetting into mapsactivity

--- app/src/main/java/com/example/benja/geopix/MapsActivity.java
+++ app/src/main/java/com/example/benja/geopix/MapsActivity.java
@@ -3,6 +3,7 @@ package com.example.benja.geopix;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,7 +17,18 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -78,4 +90,60 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return true;
         }
     }
+
+
+    public class PhotoGetter extends AsyncTask {
+        @Override
+        protected Object doInBackground(Object[] params) {
+            try {
+                String boundary = "*****";
+
+                HttpURLConnection httpUrlConnection = null;
+
+                URL url = new URL("http://192.168.42.127:3002/images?latitude=" + params[0] + "&longitude=" + params[1]);
+//            URL url = new URL("http://geopix-bengineering.rhcloud.com/ratings/101010101010101010101010");
+
+                httpUrlConnection = (HttpURLConnection) url.openConnection();
+                httpUrlConnection.setRequestMethod("GET");
+                httpUrlConnection.setRequestProperty("Connection", "Keep-Alive");
+                httpUrlConnection.setRequestProperty("Content-Type", "application/json");
+                httpUrlConnection.setRequestProperty("Host", "android.schoolportal.gr");
+                httpUrlConnection.connect();
+
+                InputStream responseStream = new
+                        BufferedInputStream(httpUrlConnection.getInputStream());
+
+                BufferedReader responseStreamReader =
+                        new BufferedReader(new InputStreamReader(responseStream));
+
+                String line = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ((line = responseStreamReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                responseStreamReader.close();
+
+                String response = stringBuilder.toString();
+
+                JSONObject jsonObj = new JSONObject(response);
+
+                Log.d("PhotoSender Http Response", response);
+
+                responseStream.close();
+
+                httpUrlConnection.disconnect();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
 }
